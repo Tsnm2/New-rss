f9fd16311aab249bb6476e7879cad4e437aad397
anamaykashiv <kashiv.anamay001@gmail.com>
2021-07-14 13:25:25 +0530

    Update rss.py

--- rss.py
+++ rss.py
@@ -31,7 +31,7 @@ def create_feed_checker(feed_url):
         entry = FEED.entries[0]
         if entry.id != db.get_link(feed_url).link:
                        # ↓ Edit this message as your needs.
-            message = f"**{entry.title}**\n```{entry.link}```\n{entry.url}"
+            message = f"**{entry.title}**\n```{entry.link[1]}```"
             try:
                 app.send_message(log_channel, message)
                 db.update_link(feed_url, entry.id)
