047d2e003d011e07dc77bdb6b3479c2e6959b8b9
anamaykashiv <kashiv.anamay001@gmail.com>
2021-07-14 18:05:26 +0530

    Update rss.py

--- rss.py
+++ rss.py
@@ -32,9 +32,9 @@ def create_feed_checker(feed_url):
         if entry.id != db.get_link(feed_url).link:
                        # ↓ Edit this message as your needs.
             if "eztv.re" in {entry.id} or "yts.mx" in {entry.id}:   
-                message = f"/leech {entry.torrent_magneturi}"
+                message = f"/leech@jarvisleechbot {entry.torrent_magneturi}"
             else:
-                message = f"/leech {entry.link}"
+                message = f"/leech@jarvisleechbot {entry.link}"
             try:
                 app.send_message(log_channel, message)
                 db.update_link(feed_url, entry.id)
