51beffa016a0c3f1d68ab881501d2596771dad10
anamaykashiv <kashiv.anamay001@gmail.com>
2021-07-15 14:10:13 +0530

    Update rss.py

--- rss.py
+++ rss.py
@@ -34,6 +34,8 @@ def create_feed_checker(feed_url):
                        # ↓ Edit this message as your needs.
             if "eztv.re" in enid or "yts.mx" in enid:   
                 message = f"{entry.torrent_magneturi}"
+            if "watercache.nanobytes.org" in feed_url:
+                message = f"{entry.links[1]["href"]}"
             else:
                 message = f"{entry.link}"
             try:
