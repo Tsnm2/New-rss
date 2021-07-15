663f171af730d81f678a0593a3b6de075586ffe5
anamaykashiv <kashiv.anamay001@gmail.com>
2021-07-15 21:45:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-import rss
-import torrent
+import os
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+from config import Config
+
+if __name__ == "__main__" :
+    plugins = dict(
+        root="plugins"
+    )
+    app = pyrogram.Client(
+        "RSSBOT",
+        bot_token=Config.BOT_TOKEN,
+        api_id=Config.API_ID,
+        api_hash=Config.API_HASH,
+        plugins=plugins
+    )
+    app.run()
