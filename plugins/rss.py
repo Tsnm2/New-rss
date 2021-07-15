3523a3d5f2fc054ca6b2f89b9234ef45d0ab830c
anamaykashiv <kashiv.anamay001@gmail.com>
2021-07-16 00:29:18 +0530

    Update rss.py

--- plugins/rss.py
+++ plugins/rss.py
@@ -58,4 +58,3 @@ for feed_url in feed_urls:
     feed_checker = create_feed_checker(feed_url)
     scheduler.add_job(feed_checker, "interval", seconds=check_interval, max_instances=max_instances)
 scheduler.start()
-app.run()
