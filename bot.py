bb290be4d2b7f5b18450df510609a1d677b7560b
anamaykashiv <kashiv.anamay001@gmail.com>
2021-07-16 09:41:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ if __name__ == "__main__" :
     plugins = dict(
         root="plugins"
     )
-    app = Client(
+    app = pyrogram.Client(
         "RSSBOT",
         bot_token=Config.BOT_TOKEN,
         api_id=Config.API_ID,
